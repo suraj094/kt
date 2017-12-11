8c8e49398c0e190a0b29842373a6c28073f6e1f0
suraj prakash <surajprakash094@gmail.com>
2017-12-11 11:06:28 +0530

    solved

--- src/App.js
+++ src/App.js
@@ -3,19 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter : 0
+    }
+  }
+
+ IncValue = (value) => {
+    this.setState({counter: this.state.counter + value})
+ }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button Inc={1} IncVal={this.IncValue}/>
+        <Button Inc={5} IncVal={this.IncValue}/>
+        <Button Inc={10} IncVal={this.IncValue}/>
+        <Button Inc={100} IncVal={this.IncValue}/>
+        <Result res={this.state.counter} />
       </div>
     );
   }
 }
 
+
+class Button extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  countInc = () =>{
+    this.props.IncVal(this.props.Inc)
+  }
+
+  render(){
+    return(
+        <input type="button" value={this.props.Inc} onClick={this.countInc} />
+    )
+  }
+}
+
+
+const Result = (props) =>{
+  return(
+    <div>
+      <input type="text" value={props.res}/>
+    </div>
+  )
+}
 export default App;
